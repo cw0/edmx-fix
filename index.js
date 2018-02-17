7d7e17eb61fb8f3628b4bd3f4b46bf7912c54d20
chris-PC\chris <cwilson8706@gmail.com>
2018-02-16 23:10:00 -0500

    duplicate names are now printed to console

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Cheerio = require('cheerio');
 const fs = require('fs');
 
-fs.readFile('files/ClientTemplateModel.edmx.diagram', (err, fd) => {
+fs.readFile('files/ClientTemplateModel.edmx.diagram', (err, contents) => {
   if (err)
   {
     if (err.code === 'ENOENT') {
@@ -11,7 +11,22 @@ fs.readFile('files/ClientTemplateModel.edmx.diagram', (err, fd) => {
     throw err;
   } 
 
-  const $ = Cheerio.load(fd);
+  const $ = Cheerio.load(contents);
 
-  console.log($.html());
+  const shapes = $('entitytypeshape');
+
+  let entityNames = [];
+
+  for (let i = 0; i < shapes.length; i++) {
+    entityNames.push(shapes[i].attribs.entitytype);
+  }
+
+  let seen = {};
+  entityNames.map(function(name) {
+    if (seen.hasOwnProperty(name)) {
+      console.log(name);
+    } else {
+      seen[name] = true;
+    }
+  });
 });
